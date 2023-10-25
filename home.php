94d2e333edad9f2ba3d61f25be404de6f050ecaa
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-10-25 18:25:00 -0300

    intro desktop on home page

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+
+<section class="intro">
+    <div class="banner container">
+        <h1 class="intro_title">Curso de Código Limpo</h1>
+        <blockquote class="intro_quote">
+            <p>"Atenção nas pequenas coisas não é uma coisa pequena."</p>
+        </blockquote>
+        <cite class="intro_quoter">Robert C. Martin</cite>
+    </div>
+</section>
+
+<?php get_footer() ?>
