cc859c3b06aebbd7842d0660ae4f96a1f716fe6d
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-10-26 15:14:01 -0300

    footer desktop style

--- footer.php
+++ footer.php
@@ -1,4 +1,42 @@
-    <footer>BookInVideo 2024</footer>
-    <script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/index.js" type="module"></script>
+<?php
+
+$stylesheet_directory_uri = get_stylesheet_directory_uri(); 
+
+?>    
+    <footer id="footer_component">
+        <div class="container">
+            <div class="footer_wrapper">
+                <div>
+                    <a href="/"><img src="<?= $stylesheet_directory_uri; ?>/assets/images/logo.svg" alt=""></a>
+                </div>
+                <div class="footer_links">
+                    <ul>
+                        <p class="link_list_title">Mapa do site</p>
+                        <li class=""><a href="">Home</a></li>
+                        <li class=""><a href="">Minha Conta</a></li>
+                        <li class=""><a href="">Contato</a></li>
+                        <li class=""><a href="">Termos</a></li>
+                        <li class=""><a href="">Privacidade</a></li>
+                    </ul>
+                    <ul>
+                        <p class="link_list_title">Fique por dentro</p>
+                        <li class=""><a href="">instagram.com/bookinvideo</a></li>
+                        <li class=""><a href="">facebook.com/bookinvideo</a></li>
+                        <li class=""><a href="">linkedIn.com/bookinvideo</a></li>
+                    </ul>
+                    <ul>
+                        <p class="link_list_title">Ajuda</p>
+                        <li class=""><a href="">suporte@bookinvideo.com</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <p>BookInVideo © 2024. Alguns direitos reservados. CNPJ: 0001.5563/5563-00</p>
+            </div>
+        </div>
+    </footer>
+    <script src="<?= $stylesheet_directory_uri; ?>/assets/js/index.js" type="module"></script>
 </body>
 </html>
