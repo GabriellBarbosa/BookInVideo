b268792c63f85744ce2910ad2e7d548340473696
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-14 14:14:14 -0300

    rename react folder

--- functions.php
+++ functions.php
@@ -9,13 +9,13 @@ function bookinvideo_register_css() {
 add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_js');
 
 function bookinvideo_enqueue_react_js() {
-    wp_enqueue_script('react-course-content-js', get_template_directory_uri() . '/react-course-content/index.js', [], '1.0.0', true);
+    wp_enqueue_script('react-built-js', get_template_directory_uri() . '/react-built/index.js', [], '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_css');
 
 function bookinvideo_enqueue_react_css() {
-    wp_register_style('react-course-content-css', get_template_directory_uri() . '/react-course-content/index.css');
-    wp_enqueue_style('react-course-content-css');
+    wp_register_style('react-built-css', get_template_directory_uri() . '/react-built/index.css');
+    wp_enqueue_style('react-built-css');
 }
 ?>
