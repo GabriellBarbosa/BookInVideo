aea29a3dcf2fb9276bda82754e628505317deeb7
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-14 14:02:08 -0300

    add vite react and course-content

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 add_action('wp_enqueue_scripts', 'bookinvideo_register_css');
 
 function bookinvideo_register_css() {
@@ -7,4 +6,16 @@ function bookinvideo_register_css() {
     wp_enqueue_style('bookinvideo-style');
 }
 
+add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_js');
+
+function bookinvideo_enqueue_react_js() {
+    wp_enqueue_script('react-course-content-js', get_template_directory_uri() . '/react-course-content/index.js', [], '1.0.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_css');
+
+function bookinvideo_enqueue_react_css() {
+    wp_register_style('react-course-content-css', get_template_directory_uri() . '/react-course-content/index.css');
+    wp_enqueue_style('react-course-content-css');
+}
 ?>
--- slide.php
+++
@@ -1,4 +0,0 @@
-<?php /* Template Name: Slide */ ?>
-
-<h1><?= get_the_title(); ?></h1>
-
