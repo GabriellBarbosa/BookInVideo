f787f929ba6225f944c9a2bf86eeaeb41eda698a
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-03-14 10:52:39 -0300

    course build

--- course.php
+++ course.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
-    <script type="module" crossorigin src="<?= get_stylesheet_directory_uri(); ?>/react-built/index.js"></script>
-    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri(); ?>/react-built/index.css">
+    <script type="module" crossorigin src="<?= get_stylesheet_directory_uri(); ?>/course/index.js"></script>
+    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri(); ?>/course/index.css">
 </head>
 <body>
     <div id="root"></div>
--- functions.php
+++ functions.php
@@ -16,14 +16,14 @@ function bookinvideo_register_css() {
 add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_js');
 
 function bookinvideo_enqueue_react_js() {
-    wp_enqueue_script('react-built-js', get_template_directory_uri() . '/react-built/index.js', [], '1.0.0', true);
+    wp_enqueue_script('course-js', get_template_directory_uri() . '/course/index.js', [], '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_css');
 
 function bookinvideo_enqueue_react_css() {
-    wp_register_style('react-built-css', get_template_directory_uri() . '/react-built/index.css');
-    wp_enqueue_style('react-built-css');
+    wp_register_style('course-css', get_template_directory_uri() . '/course/index.css');
+    wp_enqueue_style('course-css');
 }
 
 ?>
