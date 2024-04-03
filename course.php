f2f4f3773d2a6693db44600ebddbd70d0d1987b0
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-04-03 00:57:08 -0300

    get current user witthout jwt

--- course.php
+++ course.php
@@ -1,18 +1,20 @@
 <?php /* Template Name: Course */ ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <script type="module" crossorigin src="<?= get_stylesheet_directory_uri(); ?>/course-page-in-react/index.js"></script>
-    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri(); ?>/course-page-in-react/index.css">
-</head>
-<body>
-    <div id="root"></div>
-</body>
+    <head>
+        <?php wp_head(); ?>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php bloginfo('name'); ?></title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <script type="module" crossorigin src="<?= get_stylesheet_directory_uri(); ?>/course-page-in-react/index.js"></script>
+        <link rel="stylesheet" href="<?= get_stylesheet_directory_uri(); ?>/course-page-in-react/index.css">
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
+    <?php wp_footer(); ?>
 </html>
 
--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
 <?php
 $template_directory =  get_template_directory();
+require_once($template_directory . '/utils/UserUtils.php');
 require_once($template_directory . '/custom-post-types/cpt-course.php');
 require_once($template_directory . '/custom-post-types/cpt-lesson.php');
 require_once($template_directory . '/api/get_course.php');
+require_once($template_directory . '/api/get_user.php');
 require_once($template_directory . '/custom-taxonomies/codigo-limpo-taxonomy.php');
 require_once($template_directory . '/plugin-overwrite/wc_login.php');
 require_once($template_directory . '/plugin-overwrite/wc_myaccount.php');
@@ -12,17 +14,6 @@ require_once($template_directory . '/plugin-overwrite/wc_skip_cart.php');
 require_once($template_directory . '/plugin-overwrite/jwt_token.php');
 
 add_filter('wc_add_to_cart_message', '__return_false', 10, 2);
-
-add_action('wp_login', 'bookinvideo_set_jwt_cookies');
-
-function bookinvideo_set_jwt_cookies($user_login) {
-    $jwtToken = new JwtToken();
-    $token = $jwtToken->generateSplittedToken($user_login);
-    if ($token) {
-        setcookie('header.payload', $token['headerAndPayload'], time() + 3600, COOKIEPATH, COOKIE_DOMAIN);
-        setcookie('signature', $token['signature'], time() + 3600, COOKIEPATH, COOKIE_DOMAIN, false, true); // set true to secure in production
-    }
-}
 
 add_action('after_setup_theme', 'bookinvideo_add_woocommerce_support');
 
@@ -46,7 +37,10 @@ function bookinvideo_register_css() {
 add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_js');
 
 function bookinvideo_enqueue_react_js() {
-    wp_enqueue_script('course-js', get_template_directory_uri() . '/course-page-in-react/index.js', [], '1.0.0', true);
+    wp_enqueue_script('course-js', get_template_directory_uri() . '/course-page-in-react/index.js', [], '1.0.2', true);
+    wp_localize_script('course-js', 'wp_auth',  array(
+        'nonce' => wp_create_nonce('wp_rest')
+    ));
 }
 
 add_action('wp_enqueue_scripts', 'bookinvideo_enqueue_react_css');
