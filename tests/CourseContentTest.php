5ec5fef0a746b3bc3b0d84e69067eaaa8e609500
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-04-09 13:32:35 -0300

    organize tests

--- tests/CourseContentTest.php
+++ tests/CourseContentTest.php
@@ -2,13 +2,20 @@
 declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 define('__ROOT__', dirname(dirname(__FILE__)));
-require_once __ROOT__ . '/tests/mock/CourseRepository.php';
+require_once __ROOT__ . '/src/interfaces/CourseRepository.php';
+require_once __ROOT__ . '/src/repositories/CourseRespositoryImpl.php';
 require_once __ROOT__ . '/src/entities/CourseContent.php';
 
 final class CourseContentTest extends TestCase {
     public function testGetCourseContent(): void {
-        $courseContent = new CourseContent(new MockCourseRepository());
+        $courseRepository = $this->createMock(CourseRepositoryImpl::class);
+        $courseRepository->method('getCourse')->willReturn($this->mockedCourse());
+        $courseRepository->method('getModules')->willReturn($this->mockedModule());
+        $courseRepository->method('getLessons')->willReturn($this->mockedLessons());
+
+        $courseContent = new CourseContent($courseRepository);
         $content = $courseContent->get('codigo-limpo');
+        
         $this->assertSame($content, array(
             'name' => 'Codigo limpo', 
             'slug' => 'codigo-limpo',
@@ -33,5 +40,36 @@ final class CourseContentTest extends TestCase {
                 ),
             )
         ));
+    }
+
+    private function mockedCourse() {
+        return array(
+            'name' => 'Codigo limpo', 
+            'slug' => 'codigo-limpo',
+        );
+    }
+
+    private function mockedModule() {
+        $module = new stdClass();
+        $module->name = 'Funções';
+        $module->description = '03';
+        return array($module);
+    }
+
+    private function mockedLessons() {
+        return array(
+            array(
+                'name' => 'random lesson',
+                'slug' => '0000-random-lesson',
+                'sequence' => '01',
+                'duration' => '15:05'
+            ),
+            array(
+                'name' => 'other lesson', 
+                'slug' => '0000-other-lesson',
+                'sequence' => '02',
+                'duration' => '15:05'
+            ),
+        );
     }
 }
--- tests/LessonTest.php
+++ tests/LessonTest.php
@@ -1,24 +1,36 @@
 <?php
 declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
-require_once __ROOT__ . '/tests/mock/CourseRepository.php';
-require_once __ROOT__ . '/tests/mock/UserRepository.php';
-require_once __ROOT__ . '/src/entities/Lesson.php';
+require_once __ROOT__ . '/src/interfaces/CourseRepository.php';
+require_once __ROOT__ . '/src/interfaces/UserRepository.php';
 require_once __ROOT__ . '/src/repositories/CourseRespositoryImpl.php';
 require_once __ROOT__ . '/src/repositories/UserRepositoryImpl.php';
+require_once __ROOT__ . '/src/entities/Lesson.php';
 
 final class LessonTest extends TestCase {
+    private $lesson = null;
     private $userRepository = null;
     private $courseRepository = null;
 
     protected function setUp(): void {
         $this->userRepository = $this->createMock(UserRepositoryImpl::class);
         $this->courseRepository = $this->createMock(CourseRepositoryImpl::class);
+        $this->lesson = new Lesson($this->courseRepository, $this->userRepository);
     }
 
     public function testGetLesson() {
-        $lesson = new Lesson(new MockCourseRepository(), new SubscribedUserRepository());
-        $lessonFound = $lesson->get('codigo-limpo', '0102-codigo-limpo');
+        $this->userRepository->method('isSubscribed')->willReturn(true);
+        $this->courseRepository->method('getSingleLesson')->willReturn(array(
+            'name' => 'Codigo limpo',
+            'sequence' => '0102',
+            'video_src' => 'http://vimeo.com',
+            'prev' => '0101-configuracao',
+            'next' => '0201-nomes-significativos',
+            'has_code' => 'true', 
+            'has_slide' => 'true'
+        ));
+
+        $lessonFound = $this->lesson->get('codigo-limpo', '0102-codigo-limpo');
 
         $this->assertEquals($lessonFound, array(
             'name' => 'Codigo limpo',
@@ -32,8 +44,18 @@ final class LessonTest extends TestCase {
     }
 
     public function testGetLessonUnsubscribedUser() {
-        $lesson = new Lesson(new MockCourseRepository(), new UnsubscribedUserRepository());
-        $lessonFound = $lesson->get('codigo-limpo', '0102-codigo-limpo');
+        $this->userRepository->method('isSubscribed')->willReturn(false);
+        $this->courseRepository->method('getSingleLesson')->willReturn(array(
+            'name' => 'Codigo limpo',
+            'sequence' => '0102',
+            'video_src' => 'http://vimeo.com',
+            'prev' => '0101-configuracao',
+            'next' => '0201-nomes-significativos',
+            'has_code' => 'true', 
+            'has_slide' => 'true'
+        ));
+
+        $lessonFound = $this->lesson->get('codigo-limpo', '0102-codigo-limpo');
 
         $this->assertEquals($lessonFound, array(
             'name' => 'Codigo limpo',
@@ -48,15 +70,13 @@ final class LessonTest extends TestCase {
 
     public function testCompleteLesson() {
         $this->courseRepository->method('completeLesson')->willReturn(true);
-        $lesson = new Lesson($this->courseRepository, $this->userRepository);
-        $completedFeedback = $lesson->complete('codigo-limpo', '0102-codigo-limpo');
+        $completedFeedback = $this->lesson->complete('codigo-limpo', '0102-codigo-limpo');
         $this->assertEquals($completedFeedback, true);
     }
 
     public function testCompleteLessonFail() {
         $this->courseRepository->method('completeLesson')->willReturn(false);
-        $lesson = new Lesson($this->courseRepository, $this->userRepository);
-        $completedFeedback = $lesson->complete('codigo-limpo', '0102-codigo-limpo');
+        $completedFeedback = $this->lesson->complete('codigo-limpo', '0102-codigo-limpo');
         $this->assertEquals($completedFeedback, false);
     }
 }
