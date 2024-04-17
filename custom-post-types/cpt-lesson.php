3fc39ea6840cc91fc5727ae13c3604caa3acdc47
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-04-17 10:26:05 -0300

    extract class: LessonMetaBox

--- custom-post-types/cpt-lesson.php
+++ custom-post-types/cpt-lesson.php
@@ -1,7 +1,7 @@
 <?php
-add_action('init', 'register_cpt_lesson');
+add_action('init', 'registerLessonCustomPostType');
 
-function register_cpt_lesson() {
+function registerLessonCustomPostType() {
     register_post_type('aula', array(
         'label' => 'Aula',
         'description' => 'Aula',
@@ -16,124 +16,90 @@ function register_cpt_lesson() {
     ));
 }
 
-add_action("add_meta_boxes", "add_lesson_custom_meta_box");
+add_action("add_meta_boxes", "addLessonMetaBox");
 
-function add_lesson_custom_meta_box() {
-    add_meta_box("lesson-name", "lesson fields", "custom_meta_box_markup", "aula", "normal", "high", null);
+function addLessonMetaBox() {
+    add_meta_box("lesson-metabox", "lesson fields", "displayLessonMetaBox", "aula", "normal", "high", null);
 }
 
-function custom_meta_box_markup($object) {
+function displayLessonMetaBox($lessonPost) {
     wp_nonce_field(basename(__FILE__), "meta-box-nonce");
-    ?>
-        <div>
-            <label for="name">name</label>
-            <input name="name" type="text" value="<?= get_post_meta($object->ID, "name", true); ?>">
-            <br>
-
-            <label for="sequence">sequence</label>
-            <input name="sequence" type="text" value="<?= get_post_meta($object->ID, "sequence", true); ?>">
-            <br>
-
-            <label for="slug">slug</label>
-            <input name="slug" type="text" value="<?= get_post_meta($object->ID, "slug", true); ?>">
-            <br>
-
-            <label for="video_src">video_src</label>
-            <input name="video_src" type="text" value="<?= get_post_meta($object->ID, "video_src", true); ?>">
-            <br>
-
-            <label for="duration">duration</label>
-            <input name="duration" type="text" value="<?= get_post_meta($object->ID, "duration", true); ?>">
-            <br>
-
-            <label for="prev">prev</label>
-            <input name="prev" type="text" value="<?= get_post_meta($object->ID, "prev", true); ?>">
-            <br>
-            
-            <label for="next">next</label>
-            <input name="next" type="text" value="<?= get_post_meta($object->ID, "next", true); ?>">
-            <br>
-
-            <label for="has_code">has_code</label>
-            <?php
-                $has_code_box = get_post_meta($object->ID, "has_code", true);
-                if ($has_code_box == "true") {
-                    ?>
-                        <input name="has_code" type="checkbox" value="true" checked>
-                    <?php
-                }
-                else {
-                    ?>  
-                        <input name="has_code" type="checkbox" value="true">
-                    <?php
-                }
-            ?>
-
-            <label for="has_slide">has_slide</label>
-            <?php
-                $has_slide_box = get_post_meta($object->ID, "has_slide", true);
-                if ($has_slide_box == "true") {
-                    ?>
-                        <input name="has_slide" type="checkbox" value="true" checked>
-                    <?php
-                }
-                else {
-                    ?>  
-                        <input name="has_slide" type="checkbox" value="true">
-                    <?php
-                }
-            ?>
-        </div>
-    <?php  
+    $lessonMetaBox = new LessonMetaBox();
+    $lessonMetaBox->displayFields($lessonPost);
 }
 
-add_action("save_post", "save_custom_meta_box", 10, 3);
+add_action("save_post", "saveLessonMetaBoxFields", 10, 3);
 
-function save_custom_meta_box($post_id, $post, $update) {
-    if (canEdit($post->post_type, $post_id)) {
-        $fields = array(
-            'name',
-            'sequence',
-            'slug',
-            'video_src',
-            'duration',
-            'prev',
-            'next',
-            'has_code',
-            'has_slide'
-        );
+function saveLessonMetaBoxFields($postID, $post, $update) {
+    $lessonMetaBox = new LessonMetaBox();
+    $lessonMetaBox->saveFields($postID, $post->post_type);
+}
 
-        foreach ($fields as $field) {
-            $value = "";
-            if (isset($_POST[$field])) {
-                $value = $_POST[$field];
+class LessonMetaBox {
+    private $fields = array(
+        'name',
+        'sequence',
+        'slug',
+        'video_src',
+        'duration',
+        'prev',
+        'next',
+        'code',
+        'slide',
+    );
+
+    public function displayFields($lessonPost) { ?>
+        <div>
+            <?php foreach ($this->fields as $field) { ?>
+                <label 
+                    style="display: block;"
+                    for="<?= $field; ?>"
+                ><?= $field ?></label>
+                <input 
+                    style="padding: 10px; margin: 10px 0 20px 0; width: 370px;"
+                    type="text"
+                    name="<?= $field; ?>"
+                    value="<?= get_post_meta($lessonPost->ID, $field, true); ?>"
+                >
+                <br>
+            <?php } ?>
+        </div>
+    <?php }
+
+    public function saveFields($postID, $postType) {
+        if ($this->postCanBeSaved($postType, $postID)) {
+            foreach ($this->fields as $field) {
+                update_post_meta($postID, $field, $this->getFieldValue($field));
             }
-            update_post_meta($post_id, $field, trim($value));
         }
     }
-}
 
-function canEdit($postType, $post_id) {
-    return (
-        assertEquals("aula", $postType) &&
-        nonceIsValid() && 
-        current_user_can("edit_post", $post_id) && 
-        autosaveIsDisabled()
-    );
-}
+    private function getFieldValue($field) {
+        $value = "";
+        if (isset($_POST[$field])) {
+            $value = $_POST[$field];
+        }
+        return trim($value);
+    }
 
-function nonceIsValid() {
-    return (
-        isset($_POST["meta-box-nonce"]) && 
-        wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__))
-    );
-}
+    private function postCanBeSaved($postType, $postID) {
+        return (
+            $postType == "aula" &&
+            current_user_can("edit_post", $postID) && 
+            $this->nonceIsValid() && 
+            $this->autosaveIsDisabled()
+        );
+    }
 
-function autosaveIsDisabled() {
-    return !(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE);
-}
+    private function nonceIsValid() {
+        return (
+            isset($_POST["meta-box-nonce"]) && 
+            wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__))
+        );
+    }
 
-function assertEquals($expected, $actual) {
-    return $expected == $actual;
+    private function autosaveIsDisabled() {
+        return !(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE);
+    }
 }
 ?>
