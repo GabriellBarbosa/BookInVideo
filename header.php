78eb6563e65a44a8641f85233f18bee6b04ccde9
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-04-26 15:29:30 -0300

    favicon and home titles style

--- header.php
+++ header.php
@@ -20,6 +20,7 @@ function getMyAccountButtonText() {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="<?= get_stylesheet_directory_uri(); ?>/assets/images/favicon.png">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
