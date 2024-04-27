0d51a2c13c54c176936119abfdd8687a8078fee0
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-04-27 12:17:44 -0300

    favicon react

--- course.php
+++ course.php
@@ -1,4 +1,7 @@
-<?php /* Template Name: Course */ ?>
+<?php 
+/* Template Name: Course */ 
+$stylesheetDirectoryUri = get_stylesheet_directory_uri();
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -9,8 +12,9 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-        <script type="module" crossorigin src="<?= get_stylesheet_directory_uri(); ?>/react-app/index.js"></script>
-        <link rel="stylesheet" href="<?= get_stylesheet_directory_uri(); ?>/react-app/index.css">
+    <link rel="icon" type="image/x-icon" href="<?= $stylesheetDirectoryUri; ?>/assets/images/favicon.png">
+        <script type="module" crossorigin src="<?= $stylesheetDirectoryUri; ?>/react-app/index.js"></script>
+        <link rel="stylesheet" href="<?= $stylesheetDirectoryUri; ?>/react-app/index.css">
     </head>
     <body>
         <div id="root"></div>
