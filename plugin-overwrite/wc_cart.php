853da26b539a5ac7afd4402893a9ab2b5b408f96
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-05-05 12:41:01 -0300

    change wc filter to emptyCartBeforeAdd

--- plugin-overwrite/wc_cart.php
+++ plugin-overwrite/wc_cart.php
@@ -1,9 +1,9 @@
 <?php
-add_filter('woocommerce_add_to_cart_validation', 'emptyCartBeforeAdd', 10, 5);
-function emptyCartBeforeAdd($passed, $product_id, $quantity, $variation_id = '', $variations= '') {
-    if ( ! WC()->cart->is_empty() )
-        WC()->cart->empty_cart();
-    return $passed;
+add_filter('woocommerce_add_cart_item_data', 'emptyCartBeforeAdd', 10,  3);
+function emptyCartBeforeAdd($cartItemData, $productID, $variationID) {
+    global $woocommerce;
+    $woocommerce->cart->empty_cart();
+    return $cartItemData;
 }
 
 add_filter('woocommerce_add_to_cart_redirect', 'redirectToCheckout');
