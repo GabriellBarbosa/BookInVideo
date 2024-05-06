e299b3d8cb275806843a90ca1e211c91c8f303a2
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-05-06 10:17:32 -0300

    replaces "__return_true" by function

--- plugin-overwrite/wc_checkout-account-creation.php
+++ plugin-overwrite/wc_checkout-account-creation.php
@@ -1,5 +1,7 @@
 <?php
-add_filter('woocommerce_create_account_default_checked', '__return_true');
+add_filter('woocommerce_create_account_default_checked', function($checked) {
+    return true;
+});
 
 add_filter('woocommerce_before_checkout_process', 'forceAccountCreation');
 function forceAccountCreation() {
