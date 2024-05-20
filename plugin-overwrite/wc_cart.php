800ca8de5670590a445d939b27632ef8ed47860b
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-05-20 20:41:43 -0300

    remove redirect to checkout

--- plugin-overwrite/wc_cart.php
+++ plugin-overwrite/wc_cart.php
@@ -5,9 +5,4 @@ function emptyCartBeforeAdd($cartItemData, $productID, $variationID) {
     $woocommerce->cart->empty_cart();
     return $cartItemData;
 }
-
-add_filter('woocommerce_add_to_cart_redirect', 'redirectToCheckout');
-function redirectToCheckout() {
-    return wc_get_checkout_url();
-}
 ?>
