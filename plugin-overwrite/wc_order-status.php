5314adc2f25fdf318dcacda5c5dfbe1d7cc72885
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-05-22 15:16:35 -0300

    wc order status liberado

--- plugin-overwrite/wc_order-status.php
+++ plugin-overwrite/wc_order-status.php
@@ -3,7 +3,7 @@ add_filter('wc_order_statuses', 'renamingOrderStatus');
 function renamingOrderStatus($orderStatuses) {
     foreach ($orderStatuses as $key => $status) {
         if ('wc-processing' === $key) 
-            $orderStatuses['wc-processing'] = __('Completed', 'woocommerce');
+            $orderStatuses['wc-processing'] = 'Liberado';
     }
     return $orderStatuses;
 }
