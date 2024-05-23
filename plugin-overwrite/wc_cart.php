737ab6933f3d9d65083309d9df2d24e8cdc8d293
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-05-23 10:32:51 -0300

    replace deprecated function

--- plugin-overwrite/wc_cart.php
+++ plugin-overwrite/wc_cart.php
@@ -1,8 +1,9 @@
 <?php
-add_filter('woocommerce_add_cart_item_data', 'emptyCartBeforeAdd', 10,  3);
-function emptyCartBeforeAdd($cartItemData, $productID, $variationID) {
-    global $woocommerce;
-    $woocommerce->cart->empty_cart();
+add_filter('woocommerce_add_cart_item_data', 'emptyCartBeforeAdd');
+function emptyCartBeforeAdd($cartItemData) {
+    WC()->cart->empty_cart();
     return $cartItemData;
 }
+
+add_filter('wc_add_to_cart_message_html', '__return_false', 10, 2);
 ?>
