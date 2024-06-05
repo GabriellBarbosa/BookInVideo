da0ed66a91989f91f5169a2aeb51a93f36623359
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-06-05 12:45:25 -0300

    meta pixel

--- header.php
+++ header.php
@@ -31,8 +31,27 @@ $stylesheetDirectoryUri = get_stylesheet_directory_uri();
     <link rel="manifest" href="<?= $stylesheetDirectoryUri; ?>/site.webmanifest">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#222222">
-
     <?php wp_head(); ?>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '7688025884576264'); 
+    fbq('track', 'PageView');
+    </script>
+    <noscript>
+    <img height="1" width="1" 
+    src="https://www.facebook.com/tr?id=7688025884576264&ev=PageView
+    &noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
 </head>
 <body <?php body_class(); ?>>
     <header class="header">
