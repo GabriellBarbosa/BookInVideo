99b52ebf8a4c46c021147cd3463da30e80ed777e
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-07-17 13:14:25 -0300

    add missing method to interface

--- src/Course/CourseRepository.php
+++ src/Course/CourseRepository.php
@@ -3,6 +3,7 @@ interface CourseRepository {
     public function getCourse();
     public function getModules();
     public function getLessons($moduleSlug);
+    public function getSingleLesson($lessonSlug);
     public function completeLesson($lessonSlug);
     public function getCompletedLessons();
 }
