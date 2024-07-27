ef35643a7d25a9f1f3fdf2370e332c28095614d4
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-07-27 16:37:11 -0300

    test

--- conclusion-certificate.php
+++ conclusion-certificate.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/utils/pdf-viewer/pdf.php');
 require_once(__DIR__ . '/utils/User.php');
 
 // $certificateID = get_query_var('certificate_id');
-$certificateID = $_GET['certificate_id'];
+$certificateID = 'panda';
 
 displayCertificateIfValid($certificateID);
 
