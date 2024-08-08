d8a75c71ab9a6ef80afad9ce51e3deef850c2b30
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-08-08 07:39:16 -0300

    link-share

--- header.php
+++ header.php
@@ -34,13 +34,9 @@ $stylesheetDirectoryUri = get_stylesheet_directory_uri();
 
     <meta property="og:title" content="Curso de Código Limpo" />
     <meta property="og:description" content="Aprenda a desenvolver um código expressivo, que revele propósito e evite perder horas tentando entender um código confuso." />
-    <meta property="og:type" content="website" />
     <meta property="og:url" content="https://bookinvideo.com/"/>
     <meta property="og:image" content="https://bookinvideo.com/wp-content/themes/theme/assets/images/favicon/link-share.png?reset=1">
     <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1024" />
-    <meta property="og:image:height" content="1024" />
-    <meta property="og:image:alt" content="Logo do BookInVideo: Livro e Vídeo" />
     <?php wp_head(); ?>
 
     <!-- Facebook Pixel Code -->
