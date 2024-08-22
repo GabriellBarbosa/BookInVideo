01ddaf1662d7a680fd394b274b4bc8f57e517c97
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-08-22 20:41:14 -0300

    Remove checkout unnecessary checkout fields

--- plugin-overwrite/wc_checkout.php
+++ plugin-overwrite/wc_checkout.php
@@ -3,21 +3,13 @@ add_filter('woocommerce_enable_order_notes_field', '__return_false');
 
 add_filter('woocommerce_checkout_fields', 'bookinvideo_removeNotRequiredCheckoutFields');
 function bookinvideo_removeNotRequiredCheckoutFields($fields) {
+    unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_address_1']);
     unset($fields['billing']['billing_address_2']);
-    return $fields;
-}
-
-add_filter('woocommerce_checkout_fields', 'bookinvideo_changeSomeCheckoutFieldsToRequired');
-function bookinvideo_changeSomeCheckoutFieldsToRequired($fields) {
-    $fields['billing']['billing_neighborhood']['required'] = 1;
-    $fields['billing']['billing_phone']['required'] = 1;
-    return $fields;
-}
-
-add_filter('woocommerce_checkout_fields', 'bookinvideo_changeCheckoutFieldsPriority');
-function bookinvideo_changeCheckoutFieldsPriority($fields) {
-    $fields['billing']['billing_email']['priority'] = 21;
-    $fields['billing']['billing_phone']['priority'] = 27;
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_company']);
     return $fields;
 }
 
@@ -25,23 +17,10 @@ add_filter('gettext', 'bookinvideo_changeCheckoutBillingDetailsTitle', 20, 3);
 function bookinvideo_changeCheckoutBillingDetailsTitle($translated_text, $text, $domain) {
     switch ( $translated_text ) {
         case 'Detalhes de faturamento' :
-            $translated_text = __( 'Dados', 'woocommerce' );
+            $translated_text = __( 'Seus dados', 'woocommerce' );
             break;
     }
     return $translated_text;
-}
-
-add_action('woocommerce_before_checkout_registration_form', 'bookinvideo_displayCreatePasswordTitle');
-function bookinvideo_displayCreatePasswordTitle() {
-    echo '<h3>Crie sua senha</h3>';
-}
-
-add_action('woocommerce_form_field','bookinvideo_displayTitleBeforeCheckoutAddressFields', 10, 2);
-function bookinvideo_displayTitleBeforeCheckoutAddressFields( $field, $key ){
-    if (is_checkout() && ( $key == 'billing_phone')) {
-        $field .= '<h3 class="form-row form-row-wide">Endereço de faturamento</h3>';
-    }
-    return $field;
 }
 
 add_action('woocommerce_checkout_order_review','bookinvideo_displayBookinvideoPrincingCard', 10, 2);
